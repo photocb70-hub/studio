6315b09dabb6f0023f1450ed114911655f6157b7
Carl Beevers <photocb70@gmail.com>
2025-09-06 18:36:58 +0000

    what do i need to do now

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.opticalprime.app',
+  appName: 'Optical Prime',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
